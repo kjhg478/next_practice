46f87d0c0fb6b890cf6c0957a7f3d60c253d7bd9
김종훈 <kjhg478@naver.com>
2021-11-04 21:34:19 +0900

    react-query 적용 후, api 값 받아오기

--- components/lecture/LectureList.tsx
+++ components/lecture/LectureList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import LectureItem from "./LectureItem";
+import { useQuery } from "react-query";
 
 const LectureList = (): JSX.Element => {
+  const { isLoading, data } = useQuery("lecture_list", () => {
+    return fetch("/api/lectures").then((rest) => rest.json());
+  });
+
+  console.log(data);
   return (
     <div>
       <LectureItem />
